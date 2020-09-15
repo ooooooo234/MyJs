489a2a991c365138ffeb88ccc1c7a9b5696a6bfa
ooooooo234 <70535080+ooooooo234@users.noreply.github.com>
2020-09-16 06:18:21 +0800

    Add files via upload

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-let menus;
+var menus;
 try {
   if (browser) {
     menus = browser.menus;
@@ -13,222 +13,51 @@ try {
 } catch (e) {
   browser = chrome;
 }
-menus.create({
-  id: 'fullscreen',
-  title: 'fullscreen',
-  contexts: [
-    'all'
-  ]
-});
-let create_menus;
-create_menus = browser.storage.sync.get(['list_1'], function (res) {
-  res.list_1 = res.list_1 || 'list_1';
-  menus.create({
-    id: 'list_1',
-    title: res.list_1.split('').slice(0, 25).join('') || 'list_1',
-    contexts: [
-      'all'
-    ]
+(function () {
+  browser.storage.sync.get(['fullscreen_visible'], function (res) {
+    menus.create({
+      id: 'fullscreen',
+      title: browser.i18n.getMessage('fullscreen_title') || 'fullscreen',
+      contexts: [
+        'all'
+      ],
+      visible: res['fullscreen_visible']
+    });
   });
-});
-create_menus;
-create_menus = browser.storage.sync.get(['list_2'], function (res) {
-  res.list_2 = res.list_2 || 'list_2';
-  menus.create({
-    id: 'list_2',
-    title: res.list_2.split('').slice(0, 25).join('') || 'list_2',
-    contexts: [
-      'all'
-    ]
+}) ();
+for (let number = 1; number <= 10; number++) {
+  let id = 'list_' + number;
+  browser.storage.sync.get([id], function (res) {
+    let visible = ((res[id] != null) && ((res[id] + ' ').length > 1));
+    res[id] = res[id] || id;
+    menus.create({
+      id: id,
+      title: res[id].split('').slice(0, 25).join('') || id,
+      contexts: [
+        'all'
+      ],
+      visible: visible
+    });
   });
-});
-create_menus;
-create_menus = browser.storage.sync.get(['list_3'], function (res) {
-  res.list_3 = res.list_3 || 'list_3';
-  menus.create({
-    id: 'list_3',
-    title: res.list_3.split('').slice(0, 25).join('') || 'list_3',
-    contexts: [
-      'all'
-    ]
-  });
-});
-create_menus;
-create_menus = browser.storage.sync.get(['list_4'], function (res) {
-  res.list_4 = res.list_4 || 'list_4';
-  menus.create({
-    id: 'list_4',
-    title: res.list_4.split('').slice(0, 25).join('') || 'list_4',
-    contexts: [
-      'all'
-    ]
-  });
-});
-create_menus;
-create_menus = browser.storage.sync.get(['list_5'], function (res) {
-  res.list_5 = res.list_5 || 'list_5';
-  menus.create({
-    id: 'list_5',
-    title: res.list_5.split('').slice(0, 25).join('') || 'list_5',
-    contexts: [
-      'all'
-    ]
-  });
-});
-create_menus;
-create_menus = browser.storage.sync.get(['list_6'], function (res) {
-  res.list_6 = res.list_6 || 'list_6';
-  menus.create({
-    id: 'list_6',
-    title: res.list_6.split('').slice(0, 25).join('') || 'list_6',
-    contexts: [
-      'all'
-    ]
-  });
-});
-create_menus;
-create_menus = browser.storage.sync.get(['list_7'], function (res) {
-  res.list_7 = res.list_7 || 'list_7';
-  menus.create({
-    id: 'list_7',
-    title: res.list_7.split('').slice(0, 25).join('') || 'list_7',
-    contexts: [
-      'all'
-    ]
-  });
-});
-create_menus;
-create_menus = browser.storage.sync.get(['list_8'], function (res) {
-  res.list_8 = res.list_8 || 'list_8';
-  menus.create({
-    id: 'list_8',
-    title: res.list_8.split('').slice(0, 25).join('') || 'list_8',
-    contexts: [
-      'all'
-    ]
-  });
-});
-create_menus;
-create_menus = browser.storage.sync.get(['list_9'], function (res) {
-  res.list_9 = res.list_9 || 'list_9';
-  menus.create({
-    id: 'list_9',
-    title: res.list_9.split('').slice(0, 25).join('') || 'list_9',
-    contexts: [
-      'all'
-    ]
-  });
-});
-create_menus;
-create_menus = browser.storage.sync.get(['list_10'], function (res) {
-  res.list_10 = res.list_10 || 'list_10';
-  menus.create({
-    id: 'list_10',
-    title: res.list_10.split('').slice(0, 25).join('') || 'list_10',
-    contexts: [
-      'all'
-    ]
-  });
-});
-create_menus;
+}
 menus.onClicked.addListener(function (info, tab) {
-  switch (info.menuItemId) {
-    case 'fullscreen':
-      {
-        browser.tabs.executeScript({
-          file: 'fullscreen.js'
-        });
-        break;
-      }
-    case 'list_1':
-      {
-        browser.storage.sync.get(['list_1'], function (res) {
+  if (info.menuItemId.split('list_') [1] != null) {
+    let id = info.menuItemId;
+    //let number = Number(info.menuItemId.split('list_') [1]);
+    browser.storage.sync.get([id], function (res) {
+      browser.tabs.executeScript({
+        code: res[id]
+      });
+    });
+  } else {
+    switch (info.menuItemId) {
+      case 'fullscreen':
+        {
           browser.tabs.executeScript({
-            code: res.list_1
-          })
-        });
-        break;
-      }
-    case 'list_2':
-      {
-        browser.storage.sync.get(['list_2'], function (res) {
-          browser.tabs.executeScript({
-            code: res.list_2
-          })
-        });
-        break;
-      }
-    case 'list_3':
-      {
-        browser.storage.sync.get(['list_3'], function (res) {
-          browser.tabs.executeScript({
-            code: res.list_3
-          })
-        });
-        break;
-      }
-    case 'list_4':
-      {
-        browser.storage.sync.get(['list_4'], function (res) {
-          browser.tabs.executeScript({
-            code: res.list_4
-          })
-        });
-        break;
-      }
-    case 'list_5':
-      {
-        browser.storage.sync.get(['list_5'], function (res) {
-          browser.tabs.executeScript({
-            code: res.list_5
-          })
-        });
-        break;
-      }
-    case 'list_6':
-      {
-        browser.storage.sync.get(['list_6'], function (res) {
-          browser.tabs.executeScript({
-            code: res.list_6
-          })
-        });
-        break;
-      }
-    case 'list_7':
-      {
-        browser.storage.sync.get(['list_7'], function (res) {
-          browser.tabs.executeScript({
-            code: res.list_7
-          })
-        });
-        break;
-      }
-    case 'list_8':
-      {
-        browser.storage.sync.get(['list_8'], function (res) {
-          browser.tabs.executeScript({
-            code: res.list_8
-          })
-        });
-        break;
-      }
-    case 'list_9':
-      {
-        browser.storage.sync.get(['list_9'], function (res) {
-          browser.tabs.executeScript({
-            code: res.list_9
-          })
-        });
-        break;
-      }
-    case 'list_10':
-      {
-        browser.storage.sync.get(['list_10'], function (res) {
-          browser.tabs.executeScript({
-            code: res.list_10
-          })
-        });
-        break;
-      }
+            file: 'fullscreen.js'
+          });
+          break;
+        }
+    }
   }
 });
--- pop.js
+++ pop.js
@@ -1,10 +1,126 @@
+let menus;
+try {
+  if (browser) {
+    menus = browser.menus;
+  } else {
+    menus = chrome.contextMenus;
+  }
+} catch (e) {
+  menus = chrome.contextMenus;
+}
+try {
+  browser = browser || chrome;
+} catch (e) {
+  browser = chrome;
+}
+let unset = browser.i18n.getMessage('unset');
+let defaultcode = browser.i18n.getMessage('defaultcode');
 function submitScript() {
-  chrome.tabs.executeScript({
+  browser.tabs.executeScript({
     code: document.getElementById('code').value
   });
 }
 function open_in_tabScript() {
-  chrome.runtime.openOptionsPage();
+  browser.runtime.openOptionsPage();
 }
-document.getElementById('submit').addEventListener('click', submitScript);
-document.getElementById('open_in_tab').addEventListener('click', open_in_tabScript);
+function toSetList(e) {
+  let number = Number(e);
+  let id = 'list_' + number;
+  let value = document.getElementById('code').value;
+  toSet(id, value);
+  let visible = ((value != null) && ((value + ' ').length > 1));
+  menus.update(id, {
+    title: value.split('').slice(0, 25).join('') || id,
+    visible: visible
+  });
+}
+function toSet(id, value) {
+  let myObject = {
+  };
+  myObject[id] = value;
+  browser.storage.sync.set(myObject);
+}
+function list_click(item) {
+  if (item.checked) {
+    let button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.textContent = browser.i18n.getMessage('overwrite') || 'overwrite';
+    item.parentNode.insertBefore(button, item.nextSibling);
+    button.addEventListener('click', function () {
+      toSetList(item.parentNode.querySelector('textarea').getAttribute('name'));
+      item.parentNode.querySelector('textarea').textContent = document.getElementById('code').value || unset;
+    });
+  } else {
+    try {
+      item.parentNode.querySelector('button').remove();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+function clickEvent() {
+  let id = this.getAttribute('id');
+  if (id.split('checkbox_') [1] != null) {
+    list_click(this);
+  } else {
+    switch (id) {
+      case 'fullscreen_visible':
+        {
+          let visible = !this.checked;
+          toSet(id, visible);
+          menus.update('fullscreen', {
+            visible: visible
+          });
+          break;
+        }
+      case 'submitSave':
+        {
+          toSet('code', document.getElementById('code').value);
+          break;
+        }
+      default:
+        {
+          console.log('Error');
+          break;
+        }
+    }
+  }
+}
+function openPage() {
+  let id;
+  id = 'fullscreen_visible';
+  browser.storage.sync.get(['fullscreen_visible'], function (res) {
+    document.getElementById('fullscreen_visible').checked = !res['fullscreen_visible'] || false;
+  });
+  browser.storage.sync.get(['code'], function (res) {
+    document.getElementById('code').value = res['code'] || defaultcode;
+  });
+  for (let number = 1; number <= 10; number++) {
+    let id = 'list_' + number;
+    browser.storage.sync.get([id], function (res) {
+      document.getElementById(id).textContent = res[id] || unset;
+    });
+  }
+  document.getElementById('submit').textContent = browser.i18n.getMessage('submit_button');
+  document.getElementById('submitSave').textContent = browser.i18n.getMessage('submitSave_button');
+  document.getElementById('open_in_tab').textContent = browser.i18n.getMessage('open_in_tab_button');
+  document.getElementById('write_js_here').textContent = browser.i18n.getMessage('write_js_here_label');
+  document.getElementById('extra_options').textContent = browser.i18n.getMessage('extra_options');
+  document.getElementById('fullscreen_visible_label').textContent = browser.i18n.getMessage('fullscreen_visible_label');
+  document.getElementsByTagName('title')[0].textContent = browser.i18n.getMessage('page_title');
+  document.getElementById('submit').addEventListener('click', submitScript);
+  document.getElementById('open_in_tab').addEventListener('click', open_in_tabScript);
+  document.getElementById('submitSave').addEventListener('click', clickEvent);
+  document.getElementById('fullscreen_visible').addEventListener('change', clickEvent);
+  document.getElementById('checkbox_1').addEventListener('click', clickEvent);
+  document.getElementById('checkbox_2').addEventListener('click', clickEvent);
+  document.getElementById('checkbox_3').addEventListener('click', clickEvent);
+  document.getElementById('checkbox_4').addEventListener('click', clickEvent);
+  document.getElementById('checkbox_5').addEventListener('click', clickEvent);
+  document.getElementById('checkbox_6').addEventListener('click', clickEvent);
+  document.getElementById('checkbox_7').addEventListener('click', clickEvent);
+  document.getElementById('checkbox_8').addEventListener('click', clickEvent);
+  document.getElementById('checkbox_9').addEventListener('click', clickEvent);
+  document.getElementById('checkbox_10').addEventListener('click', clickEvent);
+}
+openPage();
